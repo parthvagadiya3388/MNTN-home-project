6dd02dddadaeff2263bca2e7746256303ea69a87
parthvagadiya3388 <parthvagadiya3388@gmail.com>
2024-10-01 10:49:02 +0530

    Feat: new mntn project setup.

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomePage from './components/header';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
